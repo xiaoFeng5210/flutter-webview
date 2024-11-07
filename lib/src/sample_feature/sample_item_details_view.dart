791aaef0bc5631159c8411c2e96592b12779080b
Eadweard <aurora.coder.zqf@gmail.com>
2024-11-07 14:04:58 +0800

    加状态

--- lib/src/sample_feature/sample_item_details_view.dart
+++ lib/src/sample_feature/sample_item_details_view.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
-/// Displays detailed information about a SampleItem.
-class SampleItemDetailsView extends StatelessWidget {
+class SampleItemDetailsView extends StatefulWidget {
   const SampleItemDetailsView({super.key});
-
   static const routeName = '/sample_item';
 
+  @override
+  State<SampleItemDetailsView> createState() => _SampleItemDetailsViewState();
+}
+
+class _SampleItemDetailsViewState extends State<SampleItemDetailsView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
