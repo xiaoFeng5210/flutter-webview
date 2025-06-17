4ebbfbc35bb2ffad68abbacb51722c0438767181
Eadweard <aurora.coder.zqf@gmail.com>
2025-06-17 12:55:48 +0800

    fix all

--- lib/src/utils/env.dart
+++ lib/src/utils/env.dart
@@ -5,7 +5,7 @@ const Environment currentEnv = env == 'dev'
     ? Environment.dev
     : Environment.prod;
 
-const String devUrl = 'https://www.baidu.com';
+const String devUrl = 'http://192.168.22.103:8092/';
 const String prodUrl = 'http://192.168.22.103:8092/';
 
 String getBaseUrl() {
